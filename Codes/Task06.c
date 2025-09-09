d097d1ada1f96f1f94b4fd74d0e91451a539ac57
S M Mahboor Hussain <K250749@nu.edu.pk>
2025-09-09 21:12:31 +0500

    Create Task06.c

---
+++ Codes/Task06.c
@@ -0,0 +1,12 @@
+/*
+Task 6: Welcome Message
+Write a C program that prints the following exactly as shown:
+Hello FASTIANS!
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello FASTIANS!");
+    return 0;
+}
